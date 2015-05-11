16c1cdba039dfc5d1aafd07000d88ff9e06df45b
Ludkaa <ludka.caniova@gmail.com>
2015-05-11 22:52:38 +0200

    Pridala som funkciu rot13.

--- String.js
+++ String.js
@@ -56,7 +56,29 @@ var s2 = 'LMN';
 console.log('mix(): ' + s1  + ' + ' +  s2 + ' -> ' + mix(s1, s2))
 
 function rot13 (x)
+{
+	var vystup = ''
+	var pole1 = 'a, b, c, d, e, f, g, h, i, j, k, l, m'
+	var pole2 = 'n, o, p, q, r, s, t, u, v, w, x, y, z'
+	for( var i = 0; i<x.length; i++)
+	{
+		var k = x[i]
+		for( var j = 0; j<pole1.length; j++)
+		{
+			if (k===pole1[j])
+			{
+				vystup = vystup + pole2[j]
+			}
+			if (k===pole2[j])
+			{
+				vystup = vystup + pole1[j]
+			}
+		}
+	}
+	return vystup
+}
 
+console.log(rot13('vwvv'))
 	
 	
 	
