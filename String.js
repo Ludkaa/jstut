b60c1559c8b097958174dbb56c96463998405f5d
Ludkaa <ludka.caniova@gmail.com>
2015-06-18 22:01:54 +0200

    Pridala som podmienku pre medzeru.

--- String.js
+++ String.js
@@ -50,37 +50,45 @@ function mix (s1, s2)
 	}
 	return vystup;
 }
-
+var vstup = 'hello world'
 var s1 = 'ABCDEFGHIJK';
 var s2 = 'LMN';
 console.log('mix(): ' + s1  + ' + ' +  s2 + ' -> ' + mix(s1, s2))
 
 function rot13 (x)
 {
+	var hodnota
 	var vystup = ''
-	var pole1 = 'a, b, c, d, e, f, g, h, i, j, k, l, m'
-	var pole2 = 'n, o, p, q, r, s, t, u, v, w, x, y, z'
+	var pole1 = 'abcdefghijklm'
+	var pole2 = 'nopqrstuvwxyz'
 	for( var i = 0; i<x.length; i++)
 	{
+		hodnota = false
 		var k = x[i]
 		for( var j = 0; j<pole1.length; j++)
 		{
+			
 			if (k===pole1[j])
 			{
 				vystup = vystup + pole2[j]
+				hodnota = true
 			}
 			if (k===pole2[j])
 			{
 				vystup = vystup + pole1[j]
+				hodnota = true
 			}
+		}
+		if (hodnota === false) 
+		{
+			vystup = vystup + x[i]
 		}
 	}
 	return vystup
 }
 
-console.log(rot13('vwvv'))
-	
-	
+console.log(rot13(vstup))
+console.log('rot13(): ' + vstup + ' -> ' + rot13(vstup))
 	
 	
 	
