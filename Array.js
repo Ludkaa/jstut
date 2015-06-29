fb46e95bd507309c14a4df7eaf21a56eb888a369
Ludkaa <ludka.caniova@gmail.com>
2015-06-29 21:33:56 +0200

    Pridala som funkciu vygenerujPole.

--- Array.js
+++ Array.js
@@ -9,13 +9,14 @@ var data = [
 	],
 	13,14,15
 ]
+
 /**
  * Funkcia recursion má za úlohu, vypísať obsah vstupu.
  * 
  * @param {array} a Pole na vypísanie.
  * @return {int}
  */
-function recursion(a)
+ function recursion(a)
 {
 	for(var i = 0; i < a.length; i++)
 	{
@@ -29,4 +30,50 @@ function recursion(a)
 		}
 	}
 }
-recursion (data);
+//recursion (data);
+
+/**
+ * Generuje náhodné čísla.
+ * 
+ * Generuje náhodné čísla v rozpätí od min do max.
+ * @param {int} min minimálne číslo
+ * @param {int} max maximálne číslo
+ * @return {int}
+ */
+function nahodneCislo (min,max) 
+{
+	var c = Math.floor(Math.random() * (max - min + 1) + min);
+	return c;
+}
+
+/**
+ * Generuje náhodné pole.
+ * 
+ * Generuje náhodné pole s viacerými úrovňami.
+ * @return {array}
+ */
+function vygenerujPole()
+{
+	var pole = []
+	var c = 0
+	var cislo = 0
+	for(var i = 0; i < 10; i++)
+	{
+		cislo = nahodneCislo(1,2);
+		if(cislo===1)
+		{
+			pole.push(nahodneCislo(1,100))
+		}
+		else
+		{
+			if(c < 5)
+			{
+				c = c + 1
+				pole.push(vygenerujPole())
+			}
+		}		
+	}
+	return pole
+}
+
+console.log(vygenerujPole())
